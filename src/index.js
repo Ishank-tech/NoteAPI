8b6618f728eeb24ed08e85782599728171d313a8
Ishank-tech <ishankgupta94482i@gmail.com>
2022-11-01 16:15:59 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const noteRouter = require("./routes/noteRoutes")
+const userRouter = require("./routes/userRoutes")
+const dotenv = require("dotenv")
+const cors = require("cors")
+
+dotenv.config()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+
+app.get("/", (req, res)=>{
+    res.send("Notes API");
+})
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port no. " + PORT);
+    });
+}).catch((error)=>{
+    console.log(error);
+});
